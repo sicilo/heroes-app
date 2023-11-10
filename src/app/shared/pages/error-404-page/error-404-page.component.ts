41498df43944b4baaef6622b7d7be3e4b9b7e03a
Camilo Mejía <camilo.mejia@softwareone.com>
2023-11-10 10:07:49 -0500

    feat: adding base modules

---
+++ src/app/shared/pages/error-404-page/error-404-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-404-page',
+  templateUrl: './error-404-page.component.html',
+  styles: [
+  ]
+})
+export class Error404PageComponent {
+
+}
