718bbc7d22188f7c0240588b980f70fd52587ef3
Camilo Mejía <camilo.mejia@softwareone.com>
2023-11-11 17:14:30 -0500

    feat: adding searcher

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -8,4 +12,30 @@ import { Component } from '@angular/core';
 })
 export class SearchPageComponent {
 
+  public searchInput = new FormControl();
+  public heroes: Array<Hero> = [];
+  public selectedHero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService
+  ) { }
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService
+      .getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent) {
+    if (!event.option.value) {
+      this.selectedHero = undefined;
+      return
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
+  }
 }
